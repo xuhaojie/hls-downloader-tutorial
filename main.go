711d4f67c62e76e10a15825feb85e59bc93368e5
xuhaojie <xuhaojie@hotmail.com>
2022-11-02 21:19:40 +0800

    Phase2 Step1

--- main.go
+++ main.go
@@ -4,52 +4,52 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
+	"time"
 )
 
-func main() {
-	urlBase := "https://wolongzywcdn3.com:65/20220415/3f7cISA9/"
-	blockNum := 3336
+type Block struct {
+	index int
+	url   string
+	data  []byte
+}
 
-	f, err := os.OpenFile("/tmp/video.ts", os.O_RDWR|os.O_CREATE, 0666)
+func work(b *Block) error {
+	b.data = nil
+	resp, err := http.Get(b.url)
 	if err != nil {
-		fmt.Println("Create file failed ", err)
-		return
+		fmt.Println(err)
+		return err
 	}
 
-	defer f.Close()
+	defer resp.Body.Close()
 
-	const MaxRetry = 3
-	for i := 3300; i < blockNum; i++ {
+	fmt.Printf("get %s %d\n", b.url, resp.StatusCode)
+	if resp.StatusCode == 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		b.data = body
+	}
+	return err
+}
+
+func main() {
+	beginTime := time.Now()
+
+	urlBase := "https://wolongzywcdn3.com:65/20220415/3f7cISA9/"
+	const blockNum = 3335
+	blocks := make([]Block, blockNum)
+
+	for i := 0; i < blockNum; i++ {
 		url := urlBase + fmt.Sprintf("0%d", i) + ".ts"
-		retry := 0
-		for retry < MaxRetry {
-			retry++
-			resp, err := http.Get(url)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Printf("Get block %d from %s %d\n", i, url, resp.StatusCode)
-				defer resp.Body.Close()
-				if resp.StatusCode == 200 {
-					body, err := ioutil.ReadAll(resp.Body)
-					if err != nil {
-						fmt.Println(err)
-						return
-					} else {
-						_, err := f.Write(body)
-						if err != nil {
-							fmt.Println(err.Error())
-							return
-						}
-					}
-					break
-				}
-			}
-		}
-		if retry >= MaxRetry {
-			fmt.Printf("Get block %d from %s failed for %d times.\n", i, url, retry)
-			break
-		}
+		blocks[i].index = i
+		blocks[i].url = url
+		go work(&blocks[i])
 	}
+
+	endTime := time.Now()
+	fmt.Println(endTime.Sub(beginTime))
+	fmt.Println("Done.")
 }
