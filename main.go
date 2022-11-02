cbeaaa9875e129782d022015c4a51e07a78b92ac
xuhaojie <xuhaojie@hotmail.com>
2022-11-02 21:13:43 +0800

    Phase1 Step2

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
 	urlBase := "https://wolongzywcdn3.com:65/20220415/3f7cISA9/"
 	blockNum := 100
+
+	f, err := os.OpenFile("/tmp/video.ts", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer f.Close()
+
 	for i := 0; i < blockNum; i++ {
 		url := urlBase + fmt.Sprintf("0%d", i) + ".ts"
-		fmt.Println(url)
 		resp, err := http.Get(url)
-		fmt.Printf("Get block %d from %s %d\n", i, url, resp.StatusCode)
+		fmt.Printf("Get block %d form %s %d\n", i, url, resp.StatusCode)
 		if err != nil {
 			fmt.Println(err)
 		} else {
@@ -24,7 +33,11 @@ func main() {
 					fmt.Println(err)
 					return
 				} else {
-					fmt.Println(body)
+					_, err := f.Write(body)
+					if err != nil {
+						fmt.Println(err.Error())
+						return
+					}
 				}
 			}
 		}
